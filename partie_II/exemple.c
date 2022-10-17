cf19a7b73a8748affccd1772299f1bdca042ef24
abbaas <abbaas.saleem-ahamed@etu.cyu.fr>
2022-10-17 14:42:47 +0200

    push Ald

--- partie_II/exemple.c
+++ partie_II/exemple.c
@@ -9,17 +9,17 @@
 #include "nralloc.h"
 
 #define SEUIL 5
-#define SEUIL_C 10
+#define SEUIL_C -20
 #define BLANC 255
 #define NOIR 0
 #define IMAGE_GREYSCALE 0
 #define IMAGE_RGB 1
-#define LAMBDA 0.5
+#define LAMBDA 0.1
 
 const int reponse_impulsionnelle[3][3] = {{1,1,1}, {1,1,1}, {1,1,1}};
 const int Sobel_horizontal[3][3] = {{-1,0,1} , {-2,0,2}, {-1,0,1}}; //Gradient horizontal Y
 const int Sobel_vertical[3][3] = {{-1,-2,-1} , {0,0,0}, {1,2,1}}; //Gradient vertical X
-const int filtre_gaussien[3][3] = {{0.3,0.5,0.3} , {0.5,1,0.5}, {0.3,0.5,0.3}};
+const int filtre_gaussien[3][3] = {{0.2,0.3,0.2} , {0.3,1,0.3}, {0.2,0.3,0.2}};
 
 void ApplicationFiltres(byte **I, byte** Ix, byte** Iy, int i, int j);
 long detectionHarris(byte **Ix, byte** Iy, int i, int j);
@@ -31,19 +31,15 @@ int main(int argc, char** argv)
     byte **Ix;
 	byte **Iy;
     byte **In;
-    byte **Icoin;
-    //byte **Harris;
 
-    byte norme;
     long C;
     long nrh,nrl,nch,ncl;
 
-    image_ndg=LoadPGM_bmatrix("route0.pgm",&nrl,&nrh,&ncl,&nch);
+    image_ndg=LoadPGM_bmatrix("cubes.pgm",&nrl,&nrh,&ncl,&nch);
 
     Ix=bmatrix(nrl,nrh,ncl,nch);
     Iy=bmatrix(nrl,nrh,ncl,nch);
     In=bmatrix(nrl,nrh,ncl,nch);
-    Icoin=bmatrix(nrl,nrh,ncl,nch);
 
 
     for (int i = nrl + 1; i < nrh - 1; i++) 
@@ -52,19 +48,18 @@ int main(int argc, char** argv)
         {
             ApplicationFiltres(image_ndg,Ix,Iy,i,j);
             C = detectionHarris(Ix,Iy, i, j);
-            if( C > 1)
-                printf("C : %ld \n", C);
+            // if(C != 0)
+            //     printf("x : %d y : %d | C : %d\n", i,j,C);
 
-            // norme = sqrt(pow(Ix[i][j],2) + pow(Iy[i][j],2));
-            // if(norme > SEUIL) In[i][j] = 255;
-            // else In[i][j] = 0;
-           
+            if(C > SEUIL_C && C != 0)
+                In[i][j] = BLANC;
+            else
+                In[i][j] = NOIR;
         }
     }
-    // <Ix^2><Iy^2> - <IxIy> - lambda (<Ix^2> + <Iy^2>)^2
 
 
-    //SavePGM_bmatrix(In,nrl,nrh,ncl,nch,"res.pgm");
+    SavePGM_bmatrix(In,nrl,nrh,ncl,nch,"res.pgm");
 
     free_bmatrix(image_ndg,nrl,nrh,ncl,nch);
     free_bmatrix(Ix,nrl,nrh,ncl,nch);
@@ -104,27 +99,9 @@ long detectionHarris(byte **Ix, byte** Iy, int i, int j)
     Ixytmp = floor((filtre_gaussien[0][0] * (Ix[i - 1][j - 1]*Iy[i - 1][j - 1]) + filtre_gaussien[0][1] * (Ix[i - 1][j] * Iy[i - 1][j]) + filtre_gaussien[0][2] * (Ix[i - 1][j + 1] * Iy[i - 1][j + 1])
             + filtre_gaussien[1][0] * (Ix[i][j - 1] * Iy[i][j - 1]) + filtre_gaussien[1][1] * (Ix[i][j] * Iy[i][j]) + filtre_gaussien[1][2] * (Ix[i][j + 1] * Iy[i][j + 1])
             + filtre_gaussien[2][0] * (Ix[i + 1][j - 1] * Iy[i + 1][j - 1]) + filtre_gaussien[2][1] * (Ix[i + 1][j] * Iy[i + 1][j]) + filtre_gaussien[2][2] * (Ix[i + 1][j + 1] * Iy[i + 1][j + 1])) / 9);
-    
-    // if(Ixtmp > 0)
-    //     printf("Ixtmp : %hhu\n", Ixtmp);
-    // if(Iytmp > 0)
-    //     printf("Iytmp : %hhu\n", Iytmp);
-    // if(Ixytmp > 0)
-    //     printf("Ixytmp : %hhu\n", Ixytmp);
 
-    printf("Ixtmp * Iytmp : %hhu \n", Ixtmp);
-    printf("Ixytmp : %hhu \n", Ixtmp);
-    printf("Ixtmp * Iytmp : %hhu \n", Ixtmp);
 
-    if(Iytmp > 0)
-        printf("Iytmp : %hhu\n", Iytmp);
-    if(Ixytmp > 0)
-        printf("Ixytmp : %hhu\n", Ixytmp);
-
-    if ((Ixtmp * Iytmp) - Ixytmp - LAMBDA * pow((Ixtmp + Iytmp),2) > 0 )
-        printf("YES : %hhu", ((Ixtmp * Iytmp) - Ixytmp - LAMBDA * pow((Ixtmp + Iytmp),2)) );
-
-    return (Ixtmp * Iytmp - Ixytmp - LAMBDA * pow((Ixtmp + Iytmp),2) );
+    return (Ixtmp * Iytmp) - Ixytmp - (LAMBDA * pow((Ixtmp + Iytmp),2)) ;
 }
 
-    // <Ix^2><Iy^2> - <IxIy> - lambda (<Ix^2> + <Iy^2>)^2
+    // <Ix^2> <Iy^2> - <IxIy> - lambda (<Ix^2> + <Iy^2>)^2
